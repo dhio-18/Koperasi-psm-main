2a0c60b4357e4c990358236db7050859e048d95b
Dhio Eko Permana <118009106+dhio-18@users.noreply.github.com>
2025-11-26 03:30:52 +0700

    fix: return orders directly from the query in the order method

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -358,7 +358,7 @@ class AdminController extends Controller
             $ordersQuery->where('status', $statusFilter);
         }
 
-        $orders = $ordersQuery->get()
+        return $orders = $ordersQuery->get()
             ->map(function ($order) {
                 $order->date = Carbon::parse($order->created_at)->format('d-m-Y');
                 $order->time = Carbon::parse($order->created_at)->format('H:i');
